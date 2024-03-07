3f4cbadadcd95729eb0731760e9ba26c34324cb7
Pedro Santos <anda-cun@student.42lisboa.com>
2024-03-07 09:26:41 +0000

    m

---
+++ ex00/easyfind.hpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   easyfind.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: anda-cun <anda-cun@student.42lisboa.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/06 22:18:45 by anda-cun          #+#    #+#             */
+/*   Updated: 2024/03/06 22:33:17 by anda-cun         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef EASYFIND_HPP
+#define EASYFIND_HPP
+
+#include <iostream>
+#include <list>
+
+template <typename T>
+void getValue(int tofind)
+{
+    if ()
+}
+
+template <typename T>
+void easyfind(T lhs, int tofind)
+{
+    for_each(lhs.begin(), lhs.end(), getValue)
+}
+
+#endif
